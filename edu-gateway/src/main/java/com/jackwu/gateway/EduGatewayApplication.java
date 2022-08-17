4a90d0f5b3ea68cbcf04b0c5d28157da4a400ad8
jackwu12138 <2280284307@qq.com>
2022-08-18 00:43:34 +0800

    feat: 添加了 edu-gateway 模块
    添加了 edu-gateway 模块, 用于项目的网关.
     edu-gateway 模块启动测试完成.

---
+++ edu-gateway/src/main/java/com/jackwu/gateway/EduGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.jackwu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * edu-gateway 的 spring-boot 启动类
+ *
+ * @author jackwu
+ */
+@SpringBootApplication
+public class EduGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduGatewayApplication.class, args);
+    }
+}
