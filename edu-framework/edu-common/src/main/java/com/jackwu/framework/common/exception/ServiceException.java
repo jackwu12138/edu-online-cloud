46970477b8c1f864e337d6663c30db47a9d309af
jackwu12138 <2280284307@qq.com>
2022-08-18 18:15:13 +0800

    refactor: 重构错误码的枚举类
    将所有的错误码合并到 GlobalErrorCodeConstants 类中, 后续如果有添加, 应当添加到对应的模块中去, 而不是在GlobalErrorCodeConstants 中进行修改

--- edu-framework/edu-common/src/main/java/com/jackwu/framework/common/exception/ServiceException.java
+++ edu-framework/edu-common/src/main/java/com/jackwu/framework/common/exception/ServiceException.java
@@ -1,9 +1,6 @@
 package com.jackwu.framework.common.exception;
 
 import com.jackwu.framework.common.exception.constants.GlobalErrorCodeConstants;
-import com.jackwu.framework.common.exception.constants.ServerErrorCodeConstants;
-import com.jackwu.framework.common.exception.constants.ThirdPartyErrorCodeConstants;
-import com.jackwu.framework.common.exception.constants.UserErrorCodeConstants;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,9 +19,6 @@ public class ServiceException extends RuntimeException{
      * 所抛出的错误的错误代码
      *
      * @see GlobalErrorCodeConstants
-     * @see UserErrorCodeConstants
-     * @see ServerErrorCodeConstants
-     * @see ThirdPartyErrorCodeConstants
      */
     private String code;
 
@@ -32,9 +26,6 @@ public class ServiceException extends RuntimeException{
      * 所抛出的错误的错误信息
      *
      * @see GlobalErrorCodeConstants
-     * @see UserErrorCodeConstants
-     * @see ServerErrorCodeConstants
-     * @see ThirdPartyErrorCodeConstants
      */
     private String msg;
 
--- edu-framework/edu-common/src/main/java/com/jackwu/framework/common/exception/constants/GlobalErrorCodeConstants.java
+++ edu-framework/edu-common/src/main/java/com/jackwu/framework/common/exception/constants/GlobalErrorCodeConstants.java
@@ -11,4 +11,30 @@ public interface GlobalErrorCodeConstants {
     // -------------------- 通用的成功返回码 -------------------- //
 
     ErrorCode SUCCESS = new ErrorCode("00000", "");
+
+    // -------------------- 用户端未知错误 -------------------- //
+
+    ErrorCode USER_ERROR_UNKNOWN = new ErrorCode("A0000", "用户端未知错误!");
+
+    // -------------------- 用户端通用错误 -------------------- //
+
+    ErrorCode USER_ERROR_BAD_REQUEST = new ErrorCode("A0001", "请求参数不正确");
+    ErrorCode USER_ERROR_UNAUTHORIZED = new ErrorCode("A0002", "账号未登录");
+    ErrorCode USER_ERROR_FORBIDDEN = new ErrorCode("A0003", "没有该操作权限");
+    ErrorCode USER_ERROR_NOT_FOUND = new ErrorCode("A0004", "请求未找到");
+    ErrorCode USER_ERROR_METHOD_NOT_ALLOWED = new ErrorCode("A0005", "请求方法不正确");
+    ErrorCode USER_ERROR_LOCKED = new ErrorCode("A0006", "请求失败，请稍后重试");
+    ErrorCode USER_ERROR_TOO_MANY_REQUESTS = new ErrorCode("A0007", "请求过于频繁，请稍后重试");
+
+    // -------------------- 服务端未知错误 -------------------- //
+
+    ErrorCode SERVER_ERROR_UNKNOWN = new ErrorCode("B0000", "系统未知错误!");
+
+    // -------------------- 服务端通用错误 -------------------- //
+
+    ErrorCode SERVER_INTERNAL_ERROR = new ErrorCode("B0001", "系统异常");
+
+    // -------------------- 第三方服务未知错误 -------------------- //
+
+    ErrorCode THIRD_PARTY_ERROR_UNKNOWN = new ErrorCode("C0000", "第三方未知错误!");
 }
