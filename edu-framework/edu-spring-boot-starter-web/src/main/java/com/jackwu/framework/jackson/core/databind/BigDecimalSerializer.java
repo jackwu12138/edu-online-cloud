ef3c0e5bbf96fadaba85099bcbffcdf28eafaec7
jackwu12138 <2280284307@qq.com>
2022-08-31 17:59:39 +0800

    perf: 将 BigDecimal 序列化规则中的序列化器使用 ThreadLocal 包装
    DecimalFormat 不是线程安全的类, 所以使用了 ThreadLocal 重新进行了包装

--- edu-framework/edu-spring-boot-starter-web/src/main/java/com/jackwu/framework/jackson/core/databind/BigDecimalSerializer.java
+++ edu-framework/edu-spring-boot-starter-web/src/main/java/com/jackwu/framework/jackson/core/databind/BigDecimalSerializer.java
@@ -16,14 +16,23 @@ public class BigDecimalSerializer extends JsonSerializer<BigDecimal> {
 
     public static final BigDecimalSerializer INSTANCE = new BigDecimalSerializer();
 
-    private static final DecimalFormat FORMATTER = new DecimalFormat("0.00");
-
+    /**
+     * 当 BigDecimal 属性为 null 时, 默认格式化成 ZERO_FORMAT_STRING
+     */
     private static final String ZERO_FORMAT_STRING = "0.00";
+
+    /**
+     * 日期格式化器
+     * <p>
+     * 因为{@link DecimalFormat} 不是线程安全的, 所以这里使用{@link ThreadLocal} 重新包装了一下
+     */
+    private static final ThreadLocal<DecimalFormat> FORMATTER =
+            ThreadLocal.withInitial(() -> new DecimalFormat("0.00"));
 
     @Override
     public void serialize(BigDecimal value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
         if (ObjectUtil.isNotNull(value)) {
-            gen.writeString(FORMATTER.format(value));
+            gen.writeString(FORMATTER.get().format(value));
             return;
         }
         gen.writeString(ZERO_FORMAT_STRING);
