f7b2401e59b8fffeb73bf2224a5a861ccd559112
jackwu12138 <2280284307@qq.com>
2022-09-01 14:11:00 +0800

    feat: 添加了 CorsFilter Bean
    添加了 CorsFilter Bean, 用于解决跨域的问题

--- edu-framework/edu-spring-boot-starter-web/src/main/java/com/jackwu/framework/web/config/EduWebAutoConfiguration.java
+++ edu-framework/edu-spring-boot-starter-web/src/main/java/com/jackwu/framework/web/config/EduWebAutoConfiguration.java
@@ -7,6 +7,9 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.PathMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
@@ -24,17 +27,38 @@ public class EduWebAutoConfiguration implements WebMvcConfigurer {
     @Resource
     private WebProperties webProperties;
 
+    private static <T extends Filter> FilterRegistrationBean<T> createFilterBean(T filter, Integer order) {
+        FilterRegistrationBean<T> bean = new FilterRegistrationBean<>(filter);
+        bean.setOrder(order);
+        return bean;
+    }
+
+    /**
+     * 创建 CorsFilter Bean，解决跨域问题
+     */
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterBean() {
+        // 创建 CorsConfiguration 对象
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // 设置访问源地址
+        config.addAllowedOriginPattern("*");
+        // 设置访问源请求头
+        config.addAllowedHeader("*");
+        // 设置访问源请求方法
+        config.addAllowedMethod("*");
+        // 创建 UrlBasedCorsConfigurationSource 对象
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 对接口配置跨域设置
+        source.registerCorsConfiguration("/**", config);
+        return createFilterBean(new CorsFilter(source), Integer.MIN_VALUE);
+    }
+
     /**
      * 创建 XssFilter Bean，解决 Xss 安全问题
      */
     @Bean
     public FilterRegistrationBean<XssFilter> xssFilter(XssProperties properties, PathMatcher mvcPathMatcher) {
         return createFilterBean(new XssFilter(properties, mvcPathMatcher), -102);
-    }
-
-    private static <T extends Filter> FilterRegistrationBean<T> createFilterBean(T filter, Integer order) {
-        FilterRegistrationBean<T> bean = new FilterRegistrationBean<>(filter);
-        bean.setOrder(order);
-        return bean;
     }
 }
