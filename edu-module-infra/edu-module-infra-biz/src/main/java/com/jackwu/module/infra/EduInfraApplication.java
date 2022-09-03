fef102d8bb8e5d06e8ab8f53faeb9e08023bd714
jackwu12138 <2280284307@qq.com>
2022-09-03 23:24:24 +0800

    refactor: 为新增的模块添加了启动类, 并在网关中添加了负载均衡规则
    为 edu-module-system 模块和 edu-module-infra 模块添加了启动类, 并在网关模块中添加了负载均衡到这两个服务的规则

---
+++ edu-module-infra/edu-module-infra-biz/src/main/java/com/jackwu/module/infra/EduInfraApplication.java
@@ -0,0 +1,17 @@
+package com.jackwu.module.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * edu-online-cloud 的基础设施模块
+ *
+ * @author jackwu
+ */
+@SpringBootApplication
+public class EduInfraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduInfraApplication.class, args);
+    }
+}
---
+++ edu-module-system/edu-module-system-biz/src/main/java/com/jackwu/module/system/EduSystemApplication.java
@@ -0,0 +1,17 @@
+package com.jackwu.module.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * edu-online-cloud 的系统管理模块
+ *
+ * @author jackwu
+ */
+@SpringBootApplication
+public class EduSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduSystemApplication.class, args);
+    }
+}
