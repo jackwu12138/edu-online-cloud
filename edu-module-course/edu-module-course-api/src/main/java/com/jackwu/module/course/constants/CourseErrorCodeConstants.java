cd948c4aa01cda6dcb80c564246f826e41006a2b
jackwu12138 <2280284307@qq.com>
2022-09-04 13:59:48 +0800

    refactor: 重新编排课程模块错误码
    课程模块错误码 A3000 - A3099

--- edu-module-course/edu-module-course-api/src/main/java/com/jackwu/module/course/constants/CourseErrorCodeConstants.java
+++ edu-module-course/edu-module-course-api/src/main/java/com/jackwu/module/course/constants/CourseErrorCodeConstants.java
@@ -9,10 +9,11 @@ import com.jackwu.framework.common.exception.ErrorCode;
  */
 public interface CourseErrorCodeConstants {
 
-    // -------------------- 文章相关 [ A1000 - A1099 ] -------------------- //
+    // -------------------- 文章相关 [ A3000 - A3099 ] -------------------- //
 
-    ErrorCode COURSE_ERROR_ARTICLE_TYPE_NOT_FOUND = new ErrorCode("A1000", "文章类型不存在");
-    ErrorCode COURSE_ERROR_ARTICLE_TYPE_ALREADY_EXISTS = new ErrorCode("A1001", "文章类型[{}]已经存在");
-    ErrorCode COURSE_ERROR_ARTICLE_TYPE_HAS_CHILDREN = new ErrorCode("A1002", "无法删除, 该文章类型还有文章数据");
-    ErrorCode COURSE_ERROR_ARTICLE_NOT_FOUND = new ErrorCode("A1010", "文章不存在");
+    ErrorCode COURSE_ERROR_ARTICLE_TYPE_NOT_FOUND = new ErrorCode("A3000", "文章类型不存在");
+    ErrorCode COURSE_ERROR_ARTICLE_TYPE_ALREADY_EXISTS = new ErrorCode("A3001", "文章类型[{}]已经存在");
+    ErrorCode COURSE_ERROR_ARTICLE_TYPE_HAS_CHILDREN = new ErrorCode("A3002", "无法删除, 该文章类型还有文章数据");
+
+    ErrorCode COURSE_ERROR_ARTICLE_NOT_FOUND = new ErrorCode("A3010", "文章不存在");
 }
